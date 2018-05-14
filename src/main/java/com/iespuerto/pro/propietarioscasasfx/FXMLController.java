1857c5f2d32cb1187900674c1f8f352e94dd07bf
Alexis <Alexis@SALAZAR>
2018-05-14 11:22:29 +0100

    Skip Test Maven + Ventana gráfica

--- src/main/java/com/iespuerto/pro/propietarioscasasfx/FXMLController.java
+++ src/main/java/com/iespuerto/pro/propietarioscasasfx/FXMLController.java
@@ -2,24 +2,53 @@ package com.iespuerto.pro.propietarioscasasfx;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class FXMLController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private Button btnAgregarPropietario;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private Button btnEliminarPropietario;
+    @FXML
+    private Button btnSiguientePropietario;
+    @FXML
+    private Button btnAnteriorPropietario;
+    @FXML
+    private Label lblNombrePropietario;
+    @FXML
+    private TextField txtNombrePropietario;
+    @FXML
+    private Label lblApellidosPropietario;
+    @FXML
+    private TextField txtApellidosPropietario;
+    @FXML
+    private Label lblIdPropietario;
+    @FXML
+    private TextField txtIdPropietario;
+    @FXML
+    private Label lblIdCasa;
+    @FXML
+    private Button btnAgregarCasa;
+    @FXML
+    private Button btnEliminarCasa;
+    @FXML
+    private Button btnSiguienteCasa;
+    @FXML
+    private Button btnAnteriorCasa;
+    @FXML
+    private Label lblDireccionCasa;
+    @FXML
+    private TextField txtDireccionCasa;
+    @FXML
+    private TextField txtIdCasa;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        
+    }
 }
